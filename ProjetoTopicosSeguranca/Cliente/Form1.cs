89205d7bf58097064b93bb3261e780ff6c536f72
TheDanielz3 <2171836@my.ipleiria.pt>
2018-05-21 09:56:01 +0100

    adding stuffs

--- ProjetoTopicosSeguranca/Cliente/Form1.cs
+++ ProjetoTopicosSeguranca/Cliente/Form1.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,41 @@ namespace Cliente
 {
     public partial class Form1 : Form
     {
+        private const int PORT = 9999;
+        TcpClient tcpclient = null;
+        NetworkStream networkstream = null;
+
         public Form1()
         {
             InitializeComponent();
+
+
+        }
+
+        private void buttonEnviarMensagem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Quer mesmo conectar ao Servidor?", "Conecçao", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) == DialogResult.Yes)
+            {
+                //Ativar a textbox e o button
+                textBoxMensagem.Enabled = true;
+                buttonEnviarMensagem.Enabled = true;
+
+
+                IPEndPoint endpoint = new IPEndPoint(IPAddress.Loopback, PORT);
+                tcpclient = new TcpClient();
+
+                tcpclient.Connect(endpoint);
+                networkstream = tcpclient.GetStream();
+            }
+            else
+            {
+                return;
+            }
         }
     }
 }
--- ProjetoTopicosSeguranca/Servidor/Program.cs
+++ ProjetoTopicosSeguranca/Servidor/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,26 @@ namespace Servidor
 {
     class Program
     {
+        private const int PORT = 9999;
+
         static void Main(string[] args)
         {
-        }
+            TcpListener tcpListener = null;
+            TcpClient tcpclient = null;
+            NetworkStream network_stream = null;
+
+
+            //try
+            //{
+                IPEndPoint endpoint = new IPEndPoint(IPAddress.Loopback, PORT);
+                tcpListener = new TcpListener(endpoint);
+
+                Console.WriteLine("Starting Server...");
+                tcpListener.Start();
+                Console.ReadKey();
+
+                
+            //}
+            }
     }
 }
